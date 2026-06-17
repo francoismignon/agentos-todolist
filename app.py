585d2c653aa1568a8a6e4b0096816b1fab25b51d
François Mignon <72126794+francoismignon@users.noreply.github.com>
2026-06-17 19:33:36 +0200

    feat(agent): implementation ticket #1 (#5)

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
+
+HTML_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Application</title>
+</head>
+<body>
+    <div id="app"></div>
+</body>
+</html>
+"""
+
+@app.route('/')
+def index():
+    return render_template_string(HTML_TEMPLATE)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
